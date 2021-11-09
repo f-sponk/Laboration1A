d428d5c41397ab7cabfca95104dc59e2a2757eb9
”f-sponk” <spong@net.chalmers.se>
2021-11-09 14:55:47 +0100

    Lab1A

--- Saab95.java
+++ Saab95.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public class Saab95{
+public class Saab95 extends Car{
 
     public boolean turboOn;
     public int nrDoors; // Number of doors on the car
@@ -16,33 +16,6 @@ public class Saab95{
 	    turboOn = false;
         modelName = "Saab95";
         stopEngine();
-    }
-    
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
-    }
-
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
-
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
     }
 
     public void setTurboOn(){
